434410ec0d3a8cb8de3967e42062f2f3628905f6
Yuichi Mimura <ymimura@mathworks.com>
2025-06-02 15:52:16 +0900

    edited MATLAB & yml

--- sample.m
+++ sample.m
@@ -4,6 +4,7 @@ L = 1500;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 S = 0.8 + 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
 X = S + 2*randn(size(t));
+disp(T)
 plot(1000*t,X)
 title("Signal Corrupted with Zero-Mean Random Noise")
 xlabel("t (milliseconds)")
